5a167c6efb0e98d5edb929a5c2a40e3a99e7f387
ADOLF-O <adolfoleoinfo@gmail.com>
2023-10-13 11:46:56 -0600

    rama alfa02

---
+++ HumingbirdA1/js/scroll.js
@@ -0,0 +1,77 @@
+window.addEventListener("load", main);
+
+function main(){
+
+    const imgP1 = document.querySelector(".imgP1");
+    const imgP2 = document.querySelector(".imgP2");
+    const imgP3 = document.querySelector(".imgP3");
+    const imgP4 = document.querySelector(".imgP4");
+    const pagos = document.querySelector("#pagos");
+
+    const spanC = document.querySelector(".info__span");
+  
+    let contadorC = 950;
+
+
+    
+   
+    
+    
+
+// --------------Observador--------------------------------------
+
+
+
+    const animation = (dinamic, config) =>{
+        
+        dinamic.forEach((element) => {
+            
+            if(element.isIntersecting){
+
+                if(element.target.className == "imgP1" || element.target.className == "imgP3" ){
+                    element.target.classList.add("scroll1");
+                }else if(element.target.className == "imgP2" || element.target.className == "imgP4"){
+                    element.target.classList.add("scroll2");
+                }else if(element.target.className == "info__span"){
+
+                        if(contadorC === 950){
+
+                            let intervalo = setInterval(() =>{
+                                contadorC++;
+                                spanC.textContent = "+" + contadorC ;
+                                if(contadorC == 1000){
+                                    clearInterval(intervalo);
+                                    
+                                        
+                                }
+                            }, 30)};
+                        }else if(element.target.id == "pagos"){
+                            pagos.classList.add("pagos")
+
+                        }
+                // element.target.classList.add("animacionScroll")
+                
+            }})};
+    
+    const observador = new IntersectionObserver(animation, {
+        root:null,
+        rootMargin: "0px",
+        threshold: 0.5
+    })
+
+    observador.observe(imgP1)
+    observador.observe(imgP2)
+    observador.observe(imgP3)
+    observador.observe(imgP4)
+    observador.observe(spanC)
+    observador.observe(pagos)
+
+
+}
+
+
+
+
+
+
+
